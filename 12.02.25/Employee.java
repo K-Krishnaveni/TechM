3d31e5da007f2d82711bce812d4f7519063eccd7
Krishnaveni.K <146339386+K-Krishnaveni@users.noreply.github.com>
2025-02-12 21:54:36 +0530

    Add files via upload

--- 12.02.25/Employee.java
+++ 12.02.25/Employee.java
@@ -1,10 +1,37 @@
-public class Employee {
+import java.io.*;
+class EmployeeDetail implements Serializable {
     int eno;
     String ename;
     String email;
-  public  Employee(int eno,String ename,String email){
-      this.eno = eno;
-      this.ename=ename;
-      this.email=email;
-  }
+
+     EmployeeDetail(int eno, String ename, String email) {
+        this.eno = eno;
+        this.ename = ename;
+        this.email = email;
+    }
 }
+  public class Employee {
+      public static void main(String args[]) {
+          try {
+              EmployeeDetail e1 = new EmployeeDetail(100, "Krish", "krish@gmail.com");
+              FileOutputStream fout = new FileOutputStream("f1.txt");
+              ObjectOutputStream out = new ObjectOutputStream(fout);
+              out.writeObject(e1);
+              out.flush();
+              out.close();
+              System.out.println("Serialization Success!");
+          } catch (IOException e) {
+              System.out.println("Serialization Error: " + e);
+          }
+          try {
+              ObjectInputStream in = new ObjectInputStream(new FileInputStream("f.txt"));
+              EmployeeDetail e2 = (EmployeeDetail) in.readObject();
+              in.close(); // Closing stream
+
+              System.out.println("Deserialized Student: " + e2.eno + " " + e2.ename);
+
+          } catch (IOException | ClassNotFoundException e) {
+              System.out.println("Deserialization Error: " + e);
+          }
+      }
+  }
