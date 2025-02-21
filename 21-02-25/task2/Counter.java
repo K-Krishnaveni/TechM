e945627d941bea1585db6053e13d71391f43b01b
Krishnaveni.K <146339386+K-Krishnaveni@users.noreply.github.com>
2025-02-21 13:57:31 +0530

    Add files via upload

---
+++ 21-02-25/task2/Counter.java
@@ -0,0 +1,27 @@
+package jsppack;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Counter
+ */
+@WebServlet("/counter")
+public class Counter extends HttpServlet {
+    int accesses = 0;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        accesses++;
+        out.print("Number of times this servlet has been accessed:" + accesses);
+    }
+}
